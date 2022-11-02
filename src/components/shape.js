d5fa64985a642d9bb837abef6aa651380542cff5
JYOTISINGHSUHAG <107070529+JYOTISINGHSUHAG@users.noreply.github.com>
2022-11-02 23:28:23 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,54 @@
 // declare class
 
 // export class using module.exports
+// // declare class
+
+// // export class using module.exports
+
+// class Shape{
+//     constructor(){
+//         this.color = "red";
+//     }
+
+//     drawShape(){
+//         console.log("shape drawn");
+//     }
+//     calculateArea(){
+//         console.log("Area calucted")
+//     }
+// }
+
+// module.exports = {
+//     Shape : Shape
+// }
+
+// declare class
+
+let Shape = class {
+
+    drawShape() {
+
+        console.log("Drawing Shape");
+
+    }
+
+    calculateArea() {
+
+        console.log("calculating Area");
+
+    }
+
+    color(rang) {
+
+        console.log(`filling in the ${rang}`);
+
+    }
+
+}
+
+
+ 
+
+// export class using module.exports
+
+module.exports = Shape;
